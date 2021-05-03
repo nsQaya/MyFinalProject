0b834f75278e126ab26cb1a80e5ec9578fd09a61
nsQaya <nskaya@outlook.com>
2021-05-03 21:08:32 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,30 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+
+        public static string MaintenanceTime = "Sistem bakımdaı";
+        public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Kategori için en fazla 10 ürün eklenebilir. ";
+        public static string ProductNameAlreadyExist="Bu ürün daha önceden eklenmiş";
+        public static string CategoryLimitExceeded = "Kategori limiti aşıldı";
+        public static string AuthorizationDenied ="Yetkiniz yok";
+
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+    }
+}
